35cb88ac63ebbc9c903d0c25eb78964d6587be99
ArpitDara2003 <arpitdara2003@gmail.com>
2024-12-15 13:35:53 +0530

    updating allowed hosts

--- profileApp/settings.py
+++ profileApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5hwj8g*48q0)hdh36mxlwa*p#^_^lui5z4j+!q(z#6&=xfg8sg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['profile-app-resume.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 
